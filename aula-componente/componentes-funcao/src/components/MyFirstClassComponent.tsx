4806b22c8d8e5266bc65e3343809bb9db3a73663
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 15:46:17 -0300

    adicionado arquivos da aula components MyFirstComponent

---
+++ aula-componente/componentes-funcao/src/components/MyFirstClassComponent.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class MyFirstClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My First Class Component</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus non aut eius officiis explicabo eveniet labore, vitae sequi tempora pariatur animi rerum praesentium, ab sint exercitationem. Molestiae eligendi ut sed?</p>
+            </div>
+        );
+    }
+}
+
+
+export default MyFirstClassComponent;
---
+++ aula-componente/componentes-funcao/src/components/MyFirstComponent.tsx
@@ -0,0 +1,10 @@
+const MyFirstComponent = () => {
+    return (
+        <div>
+            <h1>My First Component</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus non aut eius officiis explicabo eveniet labore, vitae sequi tempora pariatur animi rerum praesentium, ab sint exercitationem. Molestiae eligendi ut sed?</p>
+        </div>
+    );
+}
+
+export default MyFirstComponent;
