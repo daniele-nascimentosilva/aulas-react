0642b1cf935947f29d1dd6aadd70dbb40e68e6be
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 16:18:48 -0300

    adicionado arquivo da aula estilização BotaoModule

---
+++ aula-estilizacao/src/components/BotaoModule/index.tsx
@@ -0,0 +1,9 @@
+
+import "./BotaoModule.module.css"
+
+export const BotaoModule = () => {
+    return (
+        <button className={styled.myButton}>  Clique aqui  - botão module    
+            </button>
+    )
+}
