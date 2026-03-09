36642f09446eaf2d2f7f9cf0633ca70b1ed8bc01
Daniele Silva <daniele.nsilva@icloud.com>
2026-03-09 16:21:24 -0300

    adicionado arquivos da aula estilização BotaoStyled

---
+++ aula-estilizacao/src/components/BotaoStyled/Index.tsx
@@ -0,0 +1,8 @@
+import { MyButton } from "./BotaoStyled.styles"
+
+export const BotaoStyled = () => {
+    return (
+        <MyButton>  Clique aqui  - botão styled    
+            </MyButton>
+    )
+}
